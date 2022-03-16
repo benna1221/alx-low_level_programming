c01c7e73ee8ce1e709166a6422de94ca49ba480e
Biniyam <ben.bog19@gmail.com>
2022-03-16 23:44:53 +0300

    readyness

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - printing a-z 10x
+ * print_alphabet - printing a-z
  *
  * Return: void
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-int i = 0;
 char ch;
-while (i < 10)
-{
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-_putchar(ch);
+	_putchar(ch);
 }
 _putchar('\n');
-i++;
-}
 }
